41906804c4855b5ac561b70393976e1d79a04988
Leonardo M. Silva <leonardomthsilva@gmail.com>
2026-01-31 12:09:32 -0300

    Fix static data path for GitHub Pages

--- assets/index-BLqTjA9R.js
+++
@@ -1,1 +0,0 @@
-import{r as s,b as o,a3 as u}from"./index-Ba2dzVqo.js";var c=u[" useId ".trim().toString()]||(()=>{}),n=0;function i(t){const[a,r]=s.useState(c());return o(()=>{r(e=>e??String(n++))},[t]),t||(a?`radix-${a}`:"")}export{i as u};
